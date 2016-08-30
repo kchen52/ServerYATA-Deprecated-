01cdfb857a47845e8ba5678e9f8487c1bb0c0144
Kevin Chen <k.chen52@gmail.com>
2016-08-30 23:21:12 +0000

    Initial commit

---
+++ WEB-INF/classes/com/twilio/TwilioServlet.java
@@ -0,0 +1,29 @@
+package com.twilio;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.twilio.sdk.verbs.TwiMLResponse;
+import com.twilio.sdk.verbs.TwiMLException;
+import com.twilio.sdk.verbs.Message;
+
+public class TwilioServlet extends HttpServlet {
+
+	// service() responds to both GET and POST requests.
+	// You can also use doGet() or doPost()
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String bodyOfRequest = request.getParameter("Body");
+		//String myResponse = "Hello, " + request.getParameter("From");
+		TwiMLResponse twiml = new TwiMLResponse();
+		Message message = new Message(bodyOfRequest);
+		try {
+			twiml.append(message);
+		} catch (TwiMLException e) {
+			e.printStackTrace();
+		}
+
+		response.setContentType("application/xml");
+		response.getWriter().print(twiml.toXML());
+	}
+}
