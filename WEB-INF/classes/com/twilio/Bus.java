2e0205d9558e7475fb7421dcdab6f06fae1fc4f5
Kevin Chen <k.chen52@gmail.com>
2016-09-07 03:25:14 +0000

    Information from translink is now parsed, and can be selectively sent

--- WEB-INF/classes/com/twilio/Bus.java
+++ WEB-INF/classes/com/twilio/Bus.java
@@ -1,35 +1,86 @@
+package com.twilio;
+
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.*;
 
 class Bus {
-	private int vehicleNumber;
-	private int tripID;
-	private int routeNumber;
-	private Direction direction;
-	private String destination;
-	private String pattern;
-	private double longitude;
-	private double latitude;
-	private String recordedTime;
+    private int vehicleNumber;
+    private int tripId;
+    private int routeNumber;
+    private String direction;
+    private String destination;
+    private String pattern;
+    private double longitude;
+    private double latitude;
+    private String recordedTime;
 
-	public Bus() {
-		vehicleNumber = -1;
-		tripID = -1;
-		routeNumber - -1;
-		direction = Direction.NORTH;
-		destination = "hell";
-		pattern = "null";
-		longitude = 0.0;
-		latitude = 0.0;
-		recordedTime = "0000";
-	}
+    public Bus() {
+        vehicleNumber = -1;
+        tripId = -1;
+        routeNumber = -1;
+        direction = "weast";
+        destination = "hell";
+        pattern = "null";
+        longitude = 0.0;
+        latitude = 0.0;
+        recordedTime = "0000";
+    }
 
-	private enum Direction {
-		NORTH,SOUTH,EAST,WEST;
-	}
+    public void init(String initInput) {
+        // Takes translink's input, parse it, and set values accordingly
+        // Reference: initInput comes in the following format
+        // <Bus><VehicleNo>1234</VehicleNo><TripId>1234</TripId><RouteNo>1234</RouteNo><Direction>EAST</Direction>
+        // <Destination>SomePlace</Destination><Pattern>EB1</Pattern><Latitude>49.188817</Latitude><Longitude>
+        // 122.848500</Longitude><RecordedTime><02:40:48 pm</RecordedTime><RouteMap><Href>http://....</Href></RouteMap>
+        // </Bus>
 
-	public void initBus(String initInput) {
-		// Takes translink's input, parse it, and set values accordingly
-	}
+        vehicleNumber = Integer.parseInt(setValue("VehicleNo", initInput));
+        tripId = Integer.parseInt(setValue("TripId", initInput));
+        routeNumber = Integer.parseInt(setValue("RouteNo", initInput));
+        direction = setValue("Direction", initInput);
+        destination = setValue("Destination", initInput);
+        pattern = setValue("Pattern", initInput);
+        latitude = Double.parseDouble(setValue("Latitude", initInput));
+        longitude = Double.parseDouble(setValue("Longitude", initInput));
+        recordedTime = setValue("RecordedTime", initInput);
+    }
 
+    // Performs a regex search for the value specified, and returns the value(s) in those tags
+    private String setValue(String valueToSet, String input) {
+        Pattern pattern = Pattern.compile("<" + valueToSet + ">(.+?)</" + valueToSet + ">");
+        Matcher matcher = pattern.matcher(input);
+        matcher.find();
+        return(matcher.group(1));
+    }
+
+    // A bunch of getters
+    public int getVehicleNumber() {
+        return vehicleNumber;
+    }
+    public int getTripId() {
+        return tripId;
+    }
+    public int getRouteNumber() {
+        return routeNumber;
+    }
+    public String getDirection() {
+        return direction;
+    }
+    public String getDestination() {
+        return destination;
+    }
+    public String getPattern() {
+        return pattern;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+    public String getRecordedTime() {
+        return recordedTime;
+    }
 }
--- WEB-INF/classes/com/twilio/TwilioServlet.java
+++ WEB-INF/classes/com/twilio/TwilioServlet.java
@@ -18,117 +18,140 @@ import org.apache.http.message.BasicNameValuePair;
 
 import java.io.*;
 import java.net.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TwilioServlet extends HttpServlet {
-	public String ACCOUNT_SID = "NULL";
-	public String AUTH_TOKEN = "NULL";
-	public String TWILIO_NUMBER = "NULL";
-	public String TRANSLINK_API = "NULL";
+    public String ACCOUNT_SID = "NULL";
+    public String AUTH_TOKEN = "NULL";
+    public String TWILIO_NUMBER = "NULL";
+    public String TRANSLINK_API = "NULL";
 
-	private int MAX_OUTGOING_MESSAGE_LENGTH = 1550;
+    private int MAX_OUTGOING_MESSAGE_LENGTH = 1550;
 
-	// TODO: Read from a specific folder
-	public static final String CREDENTIALS_FILENAME = "/home/ubuntu/credentials";
+    // TODO: Read from a specific folder
+    public static final String CREDENTIALS_FILENAME = "/home/ubuntu/credentials";
 
-	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// TODO: Make it so credentials are only read once, upon startup... we don't need to keep
-		// rereading them.
-		// Read in credentials from credentials file
-		initializeCredentials(CREDENTIALS_FILENAME);
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // TODO: Make it so credentials are only read once, upon startup... we don't need to keep
+        // rereading them.
+        // Read in credentials from credentials file
+        initializeCredentials(CREDENTIALS_FILENAME);
 
-		// Requests come in the form: Request: 320, 341, etc.
-		String requestPhoneNumber = request.getParameter("From");
-		String bodyOfRequest = request.getParameter("Body");
+        // Requests come in the form: Request: 320, 341, etc.
+        String requestPhoneNumber = request.getParameter("From");
+        String bodyOfRequest = request.getParameter("Body");
 
-		// TODO: Use regex to check that the body follows the format
-		// if (bodyOfRequest.....)
-		String busesRequestedSplitByCommas = bodyOfRequest.split("Request: ")[1];
-		String[] busesRequested = busesRequestedSplitByCommas.split(",");
+        // TODO: Use regex to check that the body follows the format
+        // if (bodyOfRequest.....)
+        String busesRequestedSplitByCommas = bodyOfRequest.split("Request: ")[1];
+        String[] busesRequested = busesRequestedSplitByCommas.split(",");
 
-		// If no buses are requested...
-		if (busesRequested.length == 0) {
-			// Debug message for now
-			sendSMS(requestPhoneNumber, "At least one bus should be requested lol");
-		}
+        // If no buses are requested...
+        if (busesRequested.length == 0) {
+            // Debug message for now
+            sendSMS(requestPhoneNumber, "At least one bus should be requested lol");
+        }
 
-		for (int i = 0; i < busesRequested.length; i++) {
-			String currentBus = busesRequested[i];
-			String busRequestURL = "http://api.translink.ca/rttiapi/v1/buses?apikey=" + 
-				TRANSLINK_API + "&routeNo=" + currentBus;
-			sendSMS(requestPhoneNumber, getHTML(busRequestURL));
-		}
-	}
+        for (int i = 0; i < busesRequested.length; i++) {
+            String currentBus = busesRequested[i];
+            String busRequestURL = "http://api.translink.ca/rttiapi/v1/buses?apikey=" + 
+                TRANSLINK_API + "&routeNo=" + currentBus;
+            String busInformation = getHTML(busRequestURL);
 
-	private void sendSMS(String recipient, String messageToSend) {
-		// NOTE: There is a 1600 character limit imposed by Twilio, so split messages
-		// accordingly
-		ArrayList<String> splitMessages = new ArrayList<String>();
-		int lowerIndex = Math.min(MAX_OUTGOING_MESSAGE_LENGTH, messageToSend.length());
-		while (lowerIndex >= MAX_OUTGOING_MESSAGE_LENGTH) {
-			splitMessages.add(messageToSend.substring(0, MAX_OUTGOING_MESSAGE_LENGTH));
-			messageToSend = messageToSend.substring(MAX_OUTGOING_MESSAGE_LENGTH, messageToSend.length());
-			lowerIndex = Math.min(MAX_OUTGOING_MESSAGE_LENGTH, messageToSend.length());
-		}
-		// However, if the message was less than 1600 characters, we can just send it as is
-		if (splitMessages.size() == 0) {
-			splitMessages.add(messageToSend);
-		}
+            ArrayList<Bus> buses = new ArrayList<Bus>();
+            // Separate buses 
+            Pattern busPattern = Pattern.compile("<Bus>(.*?)</Bus>");
+            Matcher matcher = busPattern.matcher(busInformation);
+            while (matcher.find()) {
+                Bus bus = new Bus();
+                bus.init(matcher.group());
+                buses.add(bus);
+            }
+            StringBuilder builder = new StringBuilder();
+            for (Bus bus : buses) {
+                builder.append(bus.getVehicleNumber());
+                builder.append(":");
+                builder.append(bus.getLongitude());
+                builder.append(",");
+                builder.append(bus.getLongitude());
+                builder.append("|");
+            }
+            //currentBus.init(busRequestURL);
+            //sendSMS(requestPhoneNumber, "Initialized " + count + " buses.");
+            sendSMS(requestPhoneNumber, builder.toString());
+        }
+    }
 
-		try {
-			TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
-			Account account = client.getAccount();
-			MessageFactory messageFactory = account.getMessageFactory();
-			for (String subMessage : splitMessages) {
-				List<NameValuePair> params = new ArrayList<NameValuePair>();
-				params.add(new BasicNameValuePair("To", recipient));
-				params.add(new BasicNameValuePair("From", TWILIO_NUMBER));
-				params.add(new BasicNameValuePair("Body", subMessage));
-				Message sms = messageFactory.create(params);
-			}
-		} catch (TwilioRestException e) {
-			e.printStackTrace();
-		}
-	}
+    private void sendSMS(String recipient, String messageToSend) {
+        // NOTE: There is a 1600 character limit imposed by Twilio, so split messages
+        // accordingly
+        ArrayList<String> splitMessages = new ArrayList<String>();
+        int lowerIndex = Math.min(MAX_OUTGOING_MESSAGE_LENGTH, messageToSend.length());
+        while (lowerIndex >= MAX_OUTGOING_MESSAGE_LENGTH) {
+            splitMessages.add(messageToSend.substring(0, MAX_OUTGOING_MESSAGE_LENGTH));
+            messageToSend = messageToSend.substring(MAX_OUTGOING_MESSAGE_LENGTH, messageToSend.length());
+            lowerIndex = Math.min(MAX_OUTGOING_MESSAGE_LENGTH, messageToSend.length());
+        }
+        // However, if the message was less than 1600 characters, we can just send it as is
+        if (splitMessages.size() == 0) {
+            splitMessages.add(messageToSend);
+        }
 
-	// Taken from http://stackoverflow.com/questions/1485708/how-do-i-do-a-http-get-in-java
-	private String getHTML(String urlToRead) {
-		StringBuilder result = new StringBuilder();
-		try {
-			URL url = new URL(urlToRead);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String currentLine;
-			while ((currentLine = reader.readLine()) != null) {
-				result.append(currentLine);
-			}
-			reader.close();
-		} catch (IOException e) {
-			// No body, return an appropriate msg
-			return "NO RESULTS AVAILABLE";
-		}
-		return result.toString();
-	}
+        try {
+            TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+            Account account = client.getAccount();
+            MessageFactory messageFactory = account.getMessageFactory();
+            for (String subMessage : splitMessages) {
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("To", recipient));
+                params.add(new BasicNameValuePair("From", TWILIO_NUMBER));
+                params.add(new BasicNameValuePair("Body", subMessage));
+                Message sms = messageFactory.create(params);
+            }
+        } catch (TwilioRestException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void initializeCredentials(String fileName) {
-		// We know there will only be 3 lines, so just do this sloppy for now
-		// TODO: Make this unsloppy
-		try {
-			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-			String firstLine = bufferedReader.readLine();
-			String secondLine = bufferedReader.readLine();
-			String thirdLine = bufferedReader.readLine();
-			String fourthLine = bufferedReader.readLine();
+    // Taken from http://stackoverflow.com/questions/1485708/how-do-i-do-a-http-get-in-java
+    private String getHTML(String urlToRead) {
+        StringBuilder result = new StringBuilder();
+        try {
+            URL url = new URL(urlToRead);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection(); conn.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String currentLine;
+            while ((currentLine = reader.readLine()) != null) {
+                result.append(currentLine);
+            }
+            reader.close();
+        } catch (IOException e) {
+            // No body, return an appropriate msg
+            return "NO RESULTS AVAILABLE";
+        }
+        return result.toString();
+    }
 
-			ACCOUNT_SID = firstLine.split("ACCOUNT_SID = ")[1];
-			AUTH_TOKEN = secondLine.split("AUTH_TOKEN = ")[1];
-			TWILIO_NUMBER = thirdLine.split("TWILIO_NUMBER = ")[1];
-			TRANSLINK_API = fourthLine.split("TRANSLINK_API = ")[1];
+    private void initializeCredentials(String fileName) {
+        // We know there will only be 3 lines, so just do this sloppy for now
+        // TODO: Make this unsloppy
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String firstLine = bufferedReader.readLine();
+            String secondLine = bufferedReader.readLine();
+            String thirdLine = bufferedReader.readLine();
+            String fourthLine = bufferedReader.readLine();
 
-			bufferedReader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            ACCOUNT_SID = firstLine.split("ACCOUNT_SID = ")[1];
+            AUTH_TOKEN = secondLine.split("AUTH_TOKEN = ")[1];
+            TWILIO_NUMBER = thirdLine.split("TWILIO_NUMBER = ")[1];
+            TRANSLINK_API = fourthLine.split("TRANSLINK_API = ")[1];
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
