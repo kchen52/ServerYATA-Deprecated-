361009a2ab568dfa7447c2890162b22995e66ee9
Kevin Chen <k.chen52@gmail.com>
2016-09-08 05:04:12 +0000

    Updated build script to include Bus.java

--- WEB-INF/build.sh
+++ WEB-INF/build.sh
@@ -1,2 +1,2 @@
-javac -cp ~/apache-tomcat-6.0.45/lib/servlet-api.jar:lib/twilio-java-sdk-5.10.2-SNAPSHOT-jar-with-dependencies.jar:$CLASSPATH classes/com/twilio/TwilioServlet.java
+javac -cp ~/apache-tomcat-6.0.45/lib/servlet-api.jar:lib/twilio-java-sdk-5.10.2-SNAPSHOT-jar-with-dependencies.jar:$CLASSPATH classes/com/twilio/TwilioServlet.java classes/com/twilio/Bus.java
 #javac -cp '.:twilio-java-sdk-5.10.2-SNAPSHOT-jar-with-dependencies.jar:commons-cli-1.2.jar:httpcore-4.4.5.jar:httpcore-ab-4.4.5.jar:httpcore-nio-4.4.5.jar:$CLASSPATH' RestExamples.java
